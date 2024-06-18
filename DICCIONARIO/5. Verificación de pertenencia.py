8f80ac810085a772ed83541e215daf452460c5ec
oscar <oscariaro@iestpacora.edu.pe>
2024-06-17 22:07:14 -0500

    primer commit

---
+++ DICCIONARIO/5. Verificación de pertenencia.py
@@ -0,0 +1,9 @@
+#   Algunas otras operaciones útiles en diccionarios incluyen la verificación de pertenencia
+#   utilizando el operador in para verificar si una clave existe en el diccionario:
+
+
+STUDENTS = {"Sasuke": 19, "Naruto": 18, "Sakura": 16}
+if "Naruto" in STUDENTS:
+    print("Naruto es un estudiante de la aldea de escondida entre las hojas")
+#   imprime 
+# Naruto es un estudiante de la aldea de escondida entre las hojas
---
+++ DICCIONARIO/6. Método len().py
@@ -0,0 +1,7 @@
+#   también podemos utilizar el método len() para obtener 
+#   la cantidad de pares clave-valor en el diccionario:
+
+
+STUDENTS = {"Manolo": 16, "Messi": 25, "Ronaldo": 22}
+print(len(STUDENTS)) 
+#imprime    3
